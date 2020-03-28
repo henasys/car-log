4a7f1d2b41ee0ab07eb18903251df5c905ffb6a1
AlexLee <alexhojonglee@gmail.com>
2020-03-28 15:44:23 +0900

    extract msToTime to util.js

--- src/screen/location.js
+++ src/screen/location.js
@@ -3,24 +3,7 @@ import {StyleSheet, Text, View, FlatList} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 
 import Database from '../module/database';
-
-function msToTime(s) {
-  // Pad to 2 or 3 digits, default is 2
-  function pad(n, z) {
-    z = z || 2;
-    return ('00' + n).slice(-z);
-  }
-
-  var ms = s % 1000;
-  s = (s - ms) / 1000;
-  var secs = s % 60;
-  s = (s - secs) / 60;
-  var mins = s % 60;
-  var hrs = (s - mins) / 60;
-
-  return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
-  // return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
-}
+import {msToTime} from '../module/util';
 
 export default class LocationScreen extends React.Component {
   state = {
