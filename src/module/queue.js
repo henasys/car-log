e7529960cc16a72ab51999452bbbad6a975e7f85
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-03-30 16:38:09 +0900

    back to not default class

--- src/module/queue.js
+++ src/module/queue.js
@@ -2,7 +2,7 @@
  * original ref: https://www.tutorialspoint.com/The-Queue-Class-in-Javascript
  */
 
-class Queue {
+export class Queue {
   constructor(maxSize, isWarnPrint = false) {
     if (isNaN(maxSize)) {
       maxSize = 10;
@@ -52,6 +52,73 @@ class Queue {
   getAll() {
     return this.container;
   }
+  getLast() {
+    const length = this.container.length;
+    if (length < 1) {
+      return;
+    }
+    return this.container[length - 1];
+  }
+  getLastMinusOne() {
+    const length = this.container.length;
+    if (length < 2) {
+      return;
+    }
+    return this.container[this.container.length - 2];
+  }
 }
 
-export default Queue;
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+export class LocationQueue {
+  constructor(maxSize) {
+    this.queue = new Queue(maxSize);
+  }
+
+  enqueue(location) {
+    this.queue.enqueue(location);
+  }
+
+  averageOfLatitude() {
+    const list = this.queue.getAll().map(x => x.latitude);
+    if (list.length === 0) {
+      return 0.0;
+    }
+
+    const sum = list.reduce(reducer);
+    return sum / list.length;
+  }
+
+  averageOfLongitude() {
+    const list = this.queue.getAll().map(x => x.longitude);
+    if (list.length === 0) {
+      return 0.0;
+    }
+    const sum = list.reduce(reducer);
+    return sum / list.length;
+  }
+
+  deltaOfLatitude() {
+    const last = this.queue.getLast();
+    const minusOne = this.queue.getLastMinusOne();
+    if (!last) {
+      return 0.0;
+    }
+    if (!minusOne) {
+      return last.latitude;
+    }
+    return last.latitude - minusOne.latitude;
+  }
+
+  deltaOfLongitude() {
+    const last = this.queue.getLast();
+    const minusOne = this.queue.getLastMinusOne();
+    if (!last) {
+      return 0.0;
+    }
+    if (!minusOne) {
+      return last.longitude;
+    }
+    return last.longitude - minusOne.longitude;
+  }
+}
