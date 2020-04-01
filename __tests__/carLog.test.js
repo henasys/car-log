978da10c2656eac3964b806b736ac2454d768d29
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-04-01 17:26:39 +0900

    reduce gps error margin

--- __tests__/carLog.test.js
+++ __tests__/carLog.test.js
@@ -5,7 +5,7 @@ it('search_start_position1', () => {
   const list = carLogs;
   const velocity = '1.0';
   const period = '20';
-  const gpsErrorMargin = 500;
+  const gpsErrorMargin = 300;
   const result = searchStartPositions(list, velocity, period, gpsErrorMargin);
   showStartPositions(result);
 });
