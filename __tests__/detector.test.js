154427ab7d666ba346b5b96091808e4eaf646ec1
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-04-06 15:02:02 +0900

    test detectList

--- __tests__/detector.test.js
+++ __tests__/detector.test.js
@@ -15,4 +15,11 @@ it('detectList', () => {
   detector.detectList(locations);
   const result = detector.getResult();
   console.log('result', result);
+  expect(result.length).toEqual(3);
+  const trip1 = result[0];
+  expect(trip1.start.created).toEqual(1586146256849);
+  const trip2 = result[1];
+  expect(trip2.start.created).toEqual(1586147137405);
+  const trip3 = result[2];
+  expect(trip3.start.created).toEqual(1586150007714);
 });
