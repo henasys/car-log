39bd7a396546063fce16feee7a3c46c5c0102390
AlexLee <alexhojonglee@gmail.com>
2020-04-26 22:20:23 +0900

    move NavigationContainer to App

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {useBackButton} from '@react-navigation/native';
 
 import MyStack from './src/screen/stack';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('App constructor');
-  }
-
-  componentDidMount() {
-    console.log('App componentDidMount');
-  }
-
-  componentWillUnmount() {
-    console.log('App componentWillUnmount');
-  }
-
-  render() {
-    console.log('App render');
-    return MyStack();
-  }
+function App() {
+  const ref = React.useRef(null);
+  useBackButton(ref);
+  return (
+    <NavigationContainer
+      ref={ref}
+      onStateChange={state => {
+        console.log('New state is', state);
+      }}>
+      <MyStack />
+    </NavigationContainer>
+  );
 }
+
+export default App;
--- src/screen/stack.js
+++ src/screen/stack.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Icon} from 'react-native-elements';
 
@@ -51,89 +50,87 @@ const debugView = navigation => {
 
 export default function MyStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          component={MainScreen}
-          options={({navigation, route}) => ({
-            title: '운행 자동 기록',
-            headerRight: () => (
-              <View style={styles.menuContainer}>
-                {debugView(navigation)}
-                <Icon
-                  iconStyle={styles.menuItem}
-                  onPress={() => navigation.navigate('Share')}
-                  name="share"
-                  type="material"
-                />
-                <Icon
-                  iconStyle={styles.menuItem}
-                  onPress={() => navigation.navigate('Setting')}
-                  name="settings"
-                  type="material"
-                />
-              </View>
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="Share"
-          component={ShareScreen}
-          options={{title: '메일 전송'}}
-        />
-        <Stack.Screen
-          name="Setting"
-          component={SettingScreen}
-          options={({navigation, route}) => ({
-            title: '설정',
-            headerRight: () => (
-              <View style={styles.menuContainer}>
-                <Icon
-                  iconStyle={styles.menuItem}
-                  onPress={() => navigation.navigate('Delete')}
-                  name="delete-forever"
-                  type="material"
-                />
-              </View>
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="Delete"
-          component={DeleteScreen}
-          options={{title: '기록 삭제'}}
-        />
-        <Stack.Screen
-          name="Search"
-          component={SearchScreen}
-          options={({navigation, route}) => ({
-            title: '출발점 검출',
-          })}
-        />
-        <Stack.Screen
-          name="Location"
-          component={LocationScreen}
-          options={({navigation, route}) => ({
-            title: '위치 정보',
-          })}
-        />
-        <Stack.Screen
-          name="Trip"
-          component={TripScreen}
-          options={({navigation, route}) => ({
-            title: '운행 기록 편집',
-          })}
-        />
-        <Stack.Screen
-          name="SwipeTest"
-          component={Example}
-          options={({navigation, route}) => ({
-            title: 'Swipe Test',
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        component={MainScreen}
+        options={({navigation, route}) => ({
+          title: '운행 자동 기록',
+          headerRight: () => (
+            <View style={styles.menuContainer}>
+              {debugView(navigation)}
+              <Icon
+                iconStyle={styles.menuItem}
+                onPress={() => navigation.navigate('Share')}
+                name="share"
+                type="material"
+              />
+              <Icon
+                iconStyle={styles.menuItem}
+                onPress={() => navigation.navigate('Setting')}
+                name="settings"
+                type="material"
+              />
+            </View>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="Share"
+        component={ShareScreen}
+        options={{title: '메일 전송'}}
+      />
+      <Stack.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={({navigation, route}) => ({
+          title: '설정',
+          headerRight: () => (
+            <View style={styles.menuContainer}>
+              <Icon
+                iconStyle={styles.menuItem}
+                onPress={() => navigation.navigate('Delete')}
+                name="delete-forever"
+                type="material"
+              />
+            </View>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="Delete"
+        component={DeleteScreen}
+        options={{title: '기록 삭제'}}
+      />
+      <Stack.Screen
+        name="Search"
+        component={SearchScreen}
+        options={({navigation, route}) => ({
+          title: '출발점 검출',
+        })}
+      />
+      <Stack.Screen
+        name="Location"
+        component={LocationScreen}
+        options={({navigation, route}) => ({
+          title: '위치 정보',
+        })}
+      />
+      <Stack.Screen
+        name="Trip"
+        component={TripScreen}
+        options={({navigation, route}) => ({
+          title: '운행 기록 편집',
+        })}
+      />
+      <Stack.Screen
+        name="SwipeTest"
+        component={Example}
+        options={({navigation, route}) => ({
+          title: 'Swipe Test',
+        })}
+      />
+    </Stack.Navigator>
   );
 }
 
