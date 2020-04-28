1811f9de9ede3be2c3941112bc578e33bdba9501
AlexLee <alexhojonglee@gmail.com>
2020-04-28 23:17:26 +0900

    change the order of NavigationContainer

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import * as React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -9,26 +9,28 @@ import MyStack from './src/screen/stack';
 function App() {
   React.useEffect(() => {
     RootNavigator.isMountedRef.current = true;
-    const state = RootNavigator.navigationRef.current?.getRootState();
+    // if (RootNavigator.navigationRef.current) {
+    const state = RootNavigator.navigationRef.current.getRootState();
     console.log('init state', state);
     RootNavigator.routeNameRef.current = RootNavigator.getActiveRouteName(
       state,
     );
+    // }
     return () => (RootNavigator.isMountedRef.current = false);
   }, []);
   return (
-    <SafeAreaProvider>
-      <NavigationContainer
-        ref={RootNavigator.navigationRef}
-        onStateChange={state => {
-          console.log('New state is', state);
-          const currentRouteName = RootNavigator.getActiveRouteName(state);
-          RootNavigator.routeNameRef.current = currentRouteName;
-          console.log('currentRouteName', currentRouteName);
-        }}>
+    <NavigationContainer
+      ref={RootNavigator.navigationRef}
+      onStateChange={state => {
+        console.log('New state is', state);
+        const currentRouteName = RootNavigator.getActiveRouteName(state);
+        RootNavigator.routeNameRef.current = currentRouteName;
+        console.log('currentRouteName', currentRouteName);
+      }}>
+      <SafeAreaProvider>
         <MyStack />
-      </NavigationContainer>
-    </SafeAreaProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
