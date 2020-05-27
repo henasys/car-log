16c19cd514f71effd09f5d8f1290d2a40625c7a6
AlexLee <alexhojonglee@gmail.com>
2020-05-28 06:50:21 +0900

    back to RN 0.61.5

--- android/app/src/main/java/com/henasys/app/carlog/MainApplication.java
+++ android/app/src/main/java/com/henasys/app/carlog/MainApplication.java
@@ -4,7 +4,6 @@ import androidx.multidex.MultiDexApplication;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.rnfs.RNFSPackage;
 import com.chirag.RNMail.RNMail;
 import com.facebook.react.ReactNativeHost;
@@ -46,18 +45,15 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
 
   /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   * Loads Flipper in React Native templates.
    *
    * @param context
-   * @param reactInstanceManager
    */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
+  private static void initializeFlipper(Context context) {
     if (BuildConfig.DEBUG) {
       try {
         /*
