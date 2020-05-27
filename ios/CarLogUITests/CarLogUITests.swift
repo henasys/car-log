f2de19a4ff205f10271a4990b1d0e54d9dfeba2f
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-05-27 16:21:28 +0900

    re add SnapshotHelper.swift

--- ios/CarLogUITests/CarLogUITests.swift
+++ ios/CarLogUITests/CarLogUITests.swift
@@ -33,17 +33,9 @@ class CarLogUITests: XCTestCase {
 
     func testExample() throws {
         snapshot("0Launch")
-
+      
+      
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
