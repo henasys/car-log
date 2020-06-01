5281a6ffdf2130640d603883d493b280cad08bf5
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-06-01 09:53:08 +0900

    replace react-native-root-toast to react-native-simple-toast

--- src/module/toast.js
+++ src/module/toast.js
@@ -1,29 +1,33 @@
-import Toast from 'react-native-root-toast';
+// import Toast from 'react-native-root-toast';
+import Toast from 'react-native-simple-toast';
 
 export function toast(msg) {
-  Toast.show(msg, {
-    duration: Toast.durations.SHORT,
-    position: Toast.positions.BOTTOM,
-    shadow: true,
-    animation: true,
-  });
+  Toast.show(msg, Toast.SHORT);
+  // Toast.show(msg, {
+  //   duration: Toast.durations.SHORT,
+  //   position: Toast.positions.BOTTOM,
+  //   shadow: true,
+  //   animation: true,
+  // });
 }
 
 export function toastLong(msg) {
-  Toast.show(msg, {
-    duration: Toast.durations.LONG,
-    position: Toast.positions.BOTTOM,
-    shadow: true,
-    animation: true,
-  });
+  Toast.show(msg, Toast.LONG);
+  // Toast.show(msg, {
+  //   duration: Toast.durations.LONG,
+  //   position: Toast.positions.BOTTOM,
+  //   shadow: true,
+  //   animation: true,
+  // });
 }
 
 export function toastError(msg) {
-  Toast.show(msg, {
-    duration: Toast.durations.SHORT,
-    position: Toast.positions.BOTTOM,
-    shadow: true,
-    animation: true,
-    backgroundColor: '#fcbe22',
-  });
+  Toast.show(msg, Toast.LONG);
+  // Toast.show(msg, {
+  //   duration: Toast.durations.SHORT,
+  //   position: Toast.positions.BOTTOM,
+  //   shadow: true,
+  //   animation: true,
+  //   backgroundColor: '#fcbe22',
+  // });
 }
